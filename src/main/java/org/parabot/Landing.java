2da600c6e5c8938fbf9b1ccf02331711b5a1ee8c
Dark98 <darkaidz98@gmail.com>
2022-01-11 03:58:26 +0000

    Add -c As -clearcache Alternative

--- src/main/java/org/parabot/Landing.java
+++ src/main/java/org/parabot/Landing.java
@@ -69,6 +69,7 @@ public final class Landing {
                 case "-scriptsbin":
                     Directories.setScriptCompiledDirectory(new File(args[++i]));
                     break;
+                case "-c":
                 case "-clearcache":
                     Directories.clearCache();
                     break;
